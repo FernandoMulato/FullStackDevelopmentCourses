047efd60a57c5024a3349195ff604ed35a9c149e
FernandoMulato <juniormulato139@gmail.com>
2025-10-23 20:37:32 -0500

    End of the course day 17

---
+++ MasterCoursePython/d017_FolderExample/d017_ModuleSumExample.py
@@ -0,0 +1,3 @@
+# Function adding two numbers
+def op_sum(prm_number_1, prm_number_2):
+    return prm_number_1 + prm_number_2
---
+++ MasterCoursePython/d017_IntroductionToModularProgramming.py
@@ -0,0 +1,13 @@
+# Creating a module in Python
+# All files are modules
+
+# Namespace and Scope 
+
+# Create your own module in phyton
+
+# The module for the example is imported
+import d017_FolderExample.d017_ModuleSumExample as d017_ModuleSumExample # Nickname
+
+var_result = d017_ModuleSumExample.op_sum(50, 10);
+
+print(var_result)
